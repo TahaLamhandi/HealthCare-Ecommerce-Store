7c11342f1f33b5e95818f677f2244de9e26ca044
taha <lamhandit@gmail.com>
2025-09-13 18:37:54 +0100

    Frontend Style commit

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Toutes les Catégories - BioEkleel</title>
-    @vite(['resources/css/app.css', 'resources/js/app.jsx'])
+    @vite(['resources/css/app.css', 'resources/js/main.jsx'])
 </head>
 <body>
     <div id="app">
--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $category->name }} - BioEkleel</title>
-    @vite(['resources/css/app.css', 'resources/js/app.jsx'])
+    @vite(['resources/css/app.css', 'resources/js/main.jsx'])
 </head>
 <body>
     <div id="app">
